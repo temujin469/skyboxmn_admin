194447a4e6a04eac088a9409ecb8bb46f20f8c9e
uuganbayar <d.uuganbayar.stu@gmail.com>
2023-08-04 05:27:13 +0800

    first com

---
+++ src/api/settings/controllers/settings.ts
@@ -0,0 +1,7 @@
+/**
+ * settings controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::settings.settings');
---
+++ src/api/settings/routes/settings.ts
@@ -0,0 +1,7 @@
+/**
+ * settings router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::settings.settings');
