9663308685fd3b224c89a23be583c3a993469d5a
uuganbayar <d.uuganbayar.stu@gmail.com>
2023-08-19 18:27:53 +0800

    configuration page added

---
+++ src/api/site-configuretion/controllers/site-configuretion.ts
@@ -0,0 +1,7 @@
+/**
+ * site-configuretion controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::site-configuretion.site-configuretion');
---
+++ src/api/site-configuretion/services/site-configuretion.ts
@@ -0,0 +1,7 @@
+/**
+ * site-configuretion service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::site-configuretion.site-configuretion');
